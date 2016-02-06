61e8007c28e4b3d8a3da4ff1e5587074791110fa
Are you dreaming? <weenafk@gmail.com>
2016-02-06 17:19:39 +1100

    Setting up set 2

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,38 @@
 extern crate cryptopals;
 extern crate crypto;
 
+const SET_1: bool = false;
+const SET_2: bool = true;
+
 fn main() {
-    cryptopals::set1::c1::main();
-    cryptopals::set1::c2::main();
-    cryptopals::set1::c3::main();
-    cryptopals::set1::c4::main();
-    cryptopals::set1::c5::main();
-    cryptopals::set1::c6::main();
-    //cryptopals::set1::c7::main();
-    cryptopals::set1::c8::main();
+    do_set_1();
+    do_set_2();
+}
+
+fn do_set_1() {
+    if SET_1 {
+        use cryptopals::set1 as s;
+        s::c1::main();
+        s::c2::main();
+        s::c3::main();
+        s::c4::main();
+        s::c5::main();
+        s::c6::main();
+        s::c7::main();
+        s::c8::main();
+    }
+}
+
+fn do_set_2() {
+    if SET_2 {
+        use cryptopals::set2 as s;
+        s::c1::main();
+        s::c2::main();
+        s::c3::main();
+        s::c4::main();
+        s::c5::main();
+        s::c6::main();
+        s::c7::main();
+        s::c8::main();
+    }
 }
